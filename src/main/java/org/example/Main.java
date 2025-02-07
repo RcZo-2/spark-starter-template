b65832459a52464ff3dd19523f41be24167f8c34
RcZo-2 <sb3mk1999@gmail.com>
2025-02-07 10:26:46 +0800

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -11,13 +12,20 @@ import java.util.List;
 
 
 public final class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws Exception {
+
+
         SparkSession spark = SparkSession
                 .builder()
                 .appName("JavaSparkMongoTemplate")
-                .config("spark.mongodb.read.connection.uri", "mongodb://XXX:27017/")
-                .config("spark.mongodb.write.connection.uri", "mongodb://XXX:27017/")
+                .config("spark.mongodb.read.connection.uri", "mongodb://192.168.45.130:27017/")
+                .config("spark.mongodb.write.connection.uri", "mongodb://192.168.45.130:27017/")
                 .getOrCreate();
+
+        logger.info("---------- Spark session started ----------");
 
         Dataset<Row> df = spark
                 .read()
